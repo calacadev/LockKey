33ad6f4f28120e2e52fbb68d2590f04724b0c251
calacadev <mcskelletor@gmail.com>
2020-12-16 23:45:43 -0700

    commit 2

---
+++ app/src/main/java/com/apps/LockandKeyFi/Gen_Token.kt
@@ -0,0 +1,11 @@
+package com.apps.LockandKeyFi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Gen_Token : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.generar_token)
+    }
+}
